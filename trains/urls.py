4ad1992119a6631d5c12d8b6621504a5eae425e6
mugdha273 <mugdhasharma0327@gmail.com>
2022-04-23 23:21:03 +0530

    fix: typo

--- trains/urls.py
+++ trains/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 
 from trains import views
-from trains.views import TrainsViews, RouteViews, StationViews,Train_StatusViews, RouteStation 
+from trains.views import TrainsViews, RouteViews, StationViews,Train_StatusViews, RouteStationViews
 
 urlpatterns = [
     path('trains/', TrainsViews.as_view({'get': 'list'}), name= 'trains'),
     path('route/', RouteViews.as_view({'get': 'list'}), name= 'routes'),
     path('train_status/', Train_StatusViews.as_view({'get': 'list'}), name= 'train_Status'),
     path('station/', StationViews.as_view({'get': 'list'}), name= 'station'),
-    path('route-station/', RouteStation.as_view({'get': 'list'}), name= 'route-station'),
+    path('route-station/', RouteStationViews.as_view({'get': 'list'}), name= 'route-station'),
 ]
