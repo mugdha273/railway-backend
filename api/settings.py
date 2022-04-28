b4524c258a1dd593329d7eefe19cd10b3d2a5035
mugdha273 <mugdhasharma0327@gmail.com>
2022-04-28 22:06:52 +0530

    netlify allowed

--- api/settings.py
+++ api/settings.py
@@ -149,6 +149,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "https://book-railway-tickets.netlify.app/",
 ]
 
 ALLOWED_HOSTS = [
