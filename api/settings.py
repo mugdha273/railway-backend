2169b3a277b88a3ff49e29e997a69adfb3509d03
mugdha273 <mugdhasharma0327@gmail.com>
2022-04-28 22:36:53 +0530

    all cors allowed

--- api/settings.py
+++ api/settings.py
@@ -149,8 +149,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "https://book-railway-tickets.netlify.app/",
+
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ALLOWED_HOSTS = [
     'https://railway-backend.herokuapp.com/'
